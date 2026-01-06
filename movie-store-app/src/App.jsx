4661e76c82ec2f3b9cc6a3413c81a3fff4226e58
shay3520 <shay3520@users.noreply.github.com>
2026-01-06 20:38:14 +0200

    implement routing, css and api calls

--- movie-store-app/src/App.jsx
+++ movie-store-app/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+import "./css/App.css";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
